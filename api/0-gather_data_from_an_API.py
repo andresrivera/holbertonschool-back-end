8b35a3d30b8917e446f6c691a931a2e005473975
andresrivera <andresrivera.web@gmail.com>
2023-08-25 11:15:07 -0500

    fix code

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -50,24 +50,23 @@ STATUS CODE FOR REQUESTS
 import requests
 from sys import argv
 
+
 if __name__ == "__main__":
     API_URL = "https://jsonplaceholder.typicode.com/"
 
     user_id = argv[1]
-    response = requests.get("{}users/{}".format(API_URL, user_id),
-                            params={"_expand": "todos"})
+    response = requests.get("{}users/{}/todos".format(API_URL, user_id),
+                            params={"_expand": "user"})
 
     if response.status_code == 200:
-        user_data = response.json()
-        name = user_data["name"]
-        tasks = user_data["todos"]
-        completed_tasks = [task for task in tasks if task["completed"]]
+        data = response.json()
+        name = data[0]["user"]["name"]
+        task_done = [task for task in data if task["completed"]]
 
         print("Employee {} is done with tasks({}/{}):".format(
-            name, len(completed_tasks), len(tasks)))
-
-        for task in completed_tasks:
-            print("\t{}".format(task["title"]))
+            name, len(task_done), len(data)))
+        for task in task_done:
+            print("\t {}".format(task["title"]))
 
     else:
         print("Error: {}".format(response.status_code))
